7688c27aa87860028d1a09a59aa81d6056ab981f
Alvin Toh <alvin4292@gmail.com>
2018-03-09 00:26:25 +0800

    Create dropdown list for new order
    Lacks item to populate drop down lit

--- app/src/main/java/com/example/xuan/tungpangapp/NewOrderRequestActivity.java
+++ app/src/main/java/com/example/xuan/tungpangapp/NewOrderRequestActivity.java
@@ -2,7 +2,12 @@ package com.example.xuan.tungpangapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 public class NewOrderRequestActivity extends AppCompatActivity {
 
@@ -10,6 +15,29 @@ public class NewOrderRequestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_order_request);
-        ListView lv = (ListView) findViewById(R.id.restaurant_list);
+
+        Spinner dynamicSpinner = (Spinner) findViewById(R.id.food_Outlet);
+
+        String[] items = new String[] { "Chai Latte", "Green Tea", "Black Tea" };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, items);
+
+        dynamicSpinner.setAdapter(adapter);
+
+        dynamicSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                Log.v("item", (String) parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
     }
+
+
 }
