5787d180b5cd3147f25d67cc100e9abe328de005
Sherman K <sherman@koa.sg>
2018-03-11 00:55:43 +0800

    Use Firebase Firestore as database, storing current User's identifier and device manufacturer/model

--- app/src/main/java/com/shrmn/is416/tumpang/MyApplication.java
+++ app/src/main/java/com/shrmn/is416/tumpang/MyApplication.java
@@ -6,24 +6,86 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
+import android.util.Log;
 
 import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
 import com.estimote.coresdk.service.BeaconManager;
 import com.estimote.coresdk.recognition.packets.Beacon;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.List;
 import java.util.UUID;
 
 public class MyApplication extends Application {
 
+    private static final String TAG = "app";
+    private static final String PREF_UNIQUE_ID = "PREF_UNIQUE_ID";
+    private static final String USERS_COLLECTION = "users";
     public static BeaconManager beaconManager;
+    public static FirebaseFirestore db;
+    private static String uniqueID = null;
+    private static User user;
+    private boolean isFirstRun = false;
+
+    // Extracted from https://medium.com/@ssaurel/how-to-retrieve-an-unique-id-to-identify-android-devices-6f99fd5369eb
+    public synchronized static String id(Context context) {
+        if (uniqueID == null) {
+            SharedPreferences sharedPrefs = context.getSharedPreferences(
+                    PREF_UNIQUE_ID, Context.MODE_PRIVATE);
+            uniqueID = sharedPrefs.getString(PREF_UNIQUE_ID, null);
+            if (uniqueID == null) {
+                uniqueID = UUID.randomUUID().toString();
+                SharedPreferences.Editor editor = sharedPrefs.edit();
+                editor.putString(PREF_UNIQUE_ID, uniqueID);
+                editor.commit();
+            }
+        }
+        return uniqueID;
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        // Obtain Unique ID and save into this.uniqueID
+        id(getApplicationContext());
+        Log.d(TAG, "User Unique ID: " + uniqueID);
 
+        // Abstracted these parts into subroutines
+        initialiseBeaconSubsystem();
+        // Also loads the current User's record into this.User
+        initialiseFirebaseDatabase();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    public void showNotification(String title, String message) {
+        Intent notifyIntent = new Intent(this, MainActivity.class);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivities(this, 0,
+                new Intent[]{notifyIntent}, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+        notification.defaults |= Notification.DEFAULT_SOUND;
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
+    }
+
+    private void initialiseBeaconSubsystem() {
         beaconManager = new BeaconManager(getApplicationContext());
         beaconManager.setMonitoringListener(new BeaconManager.BeaconMonitoringListener() {
             @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
@@ -59,26 +121,45 @@ public class MyApplication extends Application {
 
             }
         });
-
     }
 
+    private void initialiseFirebaseDatabase() {
+        db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection(USERS_COLLECTION).document(uniqueID);
 
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
-    public void showNotification(String title, String message) {
-        Intent notifyIntent = new Intent(this, MainActivity.class);
-        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivities(this, 0,
-                new Intent[]{notifyIntent}, PendingIntent.FLAG_UPDATE_CURRENT);
-        Notification notification = new Notification.Builder(this)
-                .setSmallIcon(android.R.drawable.ic_dialog_info)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setAutoCancel(true)
-                .setContentIntent(pendingIntent)
-                .build();
-        notification.defaults |= Notification.DEFAULT_SOUND;
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(1, notification);
+        docRef.get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document != null && document.exists()) {
+                                user = document.toObject(User.class);
+                                Log.d(TAG, "Existing user; Loaded: " + user);
+                            } else {
+                                Log.d(TAG, "No User document with ID " + uniqueID + "; Adding to database.");
+                                addUserToDB();
+                                isFirstRun = true;
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void addUserToDB() {
+        db.collection(USERS_COLLECTION).document(uniqueID)
+                .set(new User(uniqueID))
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/shrmn/is416/tumpang/User.java
@@ -0,0 +1,115 @@
+package com.shrmn.is416.tumpang;
+
+import android.os.Build;
+
+import com.google.firebase.firestore.ServerTimestamp;
+
+import java.util.Date;
+
+public class User {
+
+    private String identifier;
+    private String telegramUsername;
+    private boolean isAdmin;
+    private String name;
+    private long createdAt;
+    private String deviceModel;
+    private String deviceManufacturer;
+
+    @ServerTimestamp
+    private Date serverTimestamp;
+
+    public User() {}
+
+    public User(String identifier, String telegramUsername, boolean isAdmin, String name, long createdAt) {
+        this.identifier = identifier;
+        this.telegramUsername = telegramUsername;
+        this.isAdmin = isAdmin;
+        this.name = name;
+        this.createdAt = createdAt;
+
+        deviceModel = Build.MODEL;
+        deviceManufacturer = Build.MANUFACTURER;
+    }
+
+    public User(String identifier, String telegramUsername, boolean isAdmin, String name) {
+        this(identifier, telegramUsername, isAdmin, name, System.currentTimeMillis());
+    }
+
+    public User(String identifier) {
+        this(identifier, null, false, null);
+    }
+
+    public User(String identifier, boolean isAdmin) {
+        this(identifier, null, isAdmin, null);
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public String getTelegramUsername() {
+        return telegramUsername;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getDeviceModel() {
+        return deviceModel;
+    }
+
+    public String getDeviceManufacturer() {
+        return deviceManufacturer;
+    }
+
+    public Date getServerTimestamp() {
+        return serverTimestamp;
+    }
+
+    public void setTelegramUsername(String telegramUsername) {
+        this.telegramUsername = telegramUsername;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return identifier.equals(user.identifier);
+    }
+
+    @Override
+    public int hashCode() {
+        return identifier.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "identifier='" + identifier + '\'' +
+                ", telegramUsername='" + telegramUsername + '\'' +
+                ", isAdmin=" + isAdmin +
+                ", name='" + name + '\'' +
+                ", createdAt=" + createdAt +
+                ", deviceModel='" + deviceModel + '\'' +
+                ", deviceManufacturer='" + deviceManufacturer + '\'' +
+                ", serverTimestamp=" + serverTimestamp +
+                '}';
+    }
+}
