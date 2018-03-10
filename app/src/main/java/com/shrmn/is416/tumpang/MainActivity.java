512ca571a2e1980095f8089ec99b8c79a5c72628
Sherman K <sherman@koa.sg>
2018-03-10 22:15:29 +0800

    Initial base project creation commit

---
+++ app/src/main/java/com/shrmn/is416/tumpang/MainActivity.java
@@ -0,0 +1,20 @@
+package com.shrmn.is416.tumpang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void newOrderRequest(View view) {
+    }
+
+    public void fulfilOrderOrderRequest(View view) {
+    }
+}
