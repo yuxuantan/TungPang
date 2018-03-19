50819bb526b90887891fda4b2e6fd95c1f2f203e
Tan Yu Xuan <yu_xuan_tan@hotmail.com>
2018-03-19 12:11:10 +0800

    FCM get token n target one user

--- app/src/main/java/com/shrmn/is416/tumpang/MyFirebaseMessagingService.java
+++ app/src/main/java/com/shrmn/is416/tumpang/MyFirebaseMessagingService.java
@@ -1,5 +1,6 @@
 package com.shrmn.is416.tumpang;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -8,6 +9,7 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -17,6 +19,7 @@ import android.util.Log;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
@@ -28,6 +31,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
      */
     // [START receive_message]
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         // ...
@@ -39,10 +43,12 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+            SendNotification(remoteMessage);
+
+
 
             if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
-//                scheduleJob();
+
             } else {
                 // Handle message within 10 seconds
 //                handleNow();
@@ -57,5 +63,38 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
+
+    }
+//    @RequiresApi(api = Build.VERSION_CODES.O)
+    void SendNotification(RemoteMessage messageBody)
+    {
+
+//        // ANDROID VERSION MUST BE 26!
+        String CHANNEL_ID = "channel_01";
+//        CharSequence name = "hi1";
+//        int importance = NotificationManager.IMPORTANCE_HIGH;
+//        NotificationChannel nChannel = new NotificationChannel(channel_id, name, importance);
+//        Intent notif = new Intent(this, MainActivity.class);
+//        PendingIntent pending = PendingIntent.getActivity(this, 0, notif, PendingIntent.FLAG_CANCEL_CURRENT); // 2nd one will override 1st notif
+//        Notification notification = new Notification.Builder(this, channel_id)
+//                .setContentTitle("New Msg")
+//                .setContentText(messageBody.toString())
+//                .setSmallIcon(R.drawable.man_only)
+//                .setChannelId(channel_id)
+//                .setContentIntent(pending)
+//                .setWhen(System.currentTimeMillis())
+//                .setShowWhen(true)
+//                .build();
+//        NotificationManager nManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//        nManager.createNotificationChannel(nChannel);
+//        nManager.notify(100, notification);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.man_only)
+                .setContentTitle("Notif")
+                .setContentText(messageBody.toString())
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(100, mBuilder.build());
+
     }
 }
