37c7127be4fe269a751241ce7a91d8c132200834
TAN SIN YIK, MARCUS _ <marcus.tan.2014@smu.edu.sg>
2018-03-28 02:11:27 +0800

    Order List and Updating FulfilOrderStatus
    Pending Order List is called via retrieveOrder()
    
    Updating Order Status is called via updateStatus()

--- app/src/main/java/com/shrmn/is416/tumpang/NewOrderRequestActivity.java
+++ app/src/main/java/com/shrmn/is416/tumpang/NewOrderRequestActivity.java
@@ -13,13 +13,19 @@ import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.SetOptions;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static android.content.ContentValues.TAG;
@@ -32,6 +38,7 @@ public class NewOrderRequestActivity extends AppCompatActivity {
     private Spinner dynamicSpinner;
     private ArrayAdapter<String> adapter;
     private Map<String, String> values;
+    private static List<Order> allOrders;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +51,10 @@ public class NewOrderRequestActivity extends AppCompatActivity {
 
         setTipAmountAdapterContents();
         retrieveLocations();
+        retrieveOrders();
+        //testing update status
+        Order order = new Order("3FyozOB3L1oyGXcVsUcA","/locations/SMU-subway","SMU-subway",1,10L,"61a3f59a-ca9e-4847-9285-5dfab55db96a","61a3f59a-ca9e-4847-9285-5dfab55db96a", new HashMap<MenuItem, Integer>(),"SIS",0);
+        updateStatus(1,5,order);
     }
 
     private void retrieveLocations() {
@@ -177,5 +188,95 @@ public class NewOrderRequestActivity extends AppCompatActivity {
 
         Intent newOrderRequestMenu = new Intent(this, NewOrderRequestMenuActivity.class);
         startActivity(newOrderRequestMenu);
+    }
+
+    private void retrieveOrders() {
+        MyApplication.db.collection("orders").whereEqualTo("status", 0).get().addOnCompleteListener(
+                new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            allOrders.clear();
+                            for (DocumentSnapshot document : task.getResult()) {
+//                                String locationID = document.getId();
+                                Map<String, Object> data = document.getData();
+
+//                                ArrayList<Drink> drinks = new ArrayList<>();
+//                                ArrayList<Food> food = new ArrayList<>();
+//                                ArrayList<MenuItem> items = new ArrayList<>();
+
+//                                Log.e(TAG, document.getId() + " => " + data);
+
+                                // ArrayList of HashMaps, 1 for each item. Key(item and qty)
+                                ArrayList<Map<String, String>> orderMenuItemsObj = (ArrayList<Map<String, String>>) data.get("menuItems");
+
+                                // Menu Item includes more details of the menu like name. what is stored in DB is links
+                                /**Can we push list of drinks into static list?**/
+                                HashMap<MenuItem, Integer> menuItems = new HashMap<>();
+                                for (Map<String, String> menuItem : orderMenuItemsObj) {
+
+                                    // Change item string reference (menuItem.get("item")) to locations table into a menu item object, and put in menuitems hashmap
+                                    String[] references = menuItem.get("item").split("/");
+                                    Log.e("ref", Arrays.toString(references));
+
+                                    String locationID = references[1];
+
+                                    String tmp[] = references[2].split("\\[|\\]");
+                                    Log.d(TAG, "onComplete: " + Arrays.toString(tmp));
+
+                                    Location location = MyApplication.locations.get(locationID);
+                                    MenuItem item = null;
+                                    if(tmp[0].equals("food")) {
+                                        Log.e("FoodItem", location.getMenu().getItems().get(Integer.parseInt(tmp[1])).toString());
+                                        item = location.getMenu().getItems().get(Integer.parseInt(tmp[1]));
+                                    } else if(tmp[0].equals("drinks")) {
+                                        Log.e("DrinkItem:", location.getMenu().getItems().get(Integer.parseInt(tmp[1])).toString());
+                                        item = location.getMenu().getItems().get(Integer.parseInt(tmp[1]));
+                                    }
+
+                                    menuItems.put(item, Integer.parseInt(String.valueOf(menuItem.get("qty"))));
+                                    Order order = new Order(document.getId(),"/locations/"+locationID,locationID,(Double)data.get("tipAmount"),0L,"nil",data.get("customerUserID")+"",menuItems,data.get("deliveryLocation")+"",0);
+                                    Log.e("Order: ",order.toString());
+                                    allOrders.add(order);
+                                }
+                            }
+//                            Log.d(TAG, "retrieveLocations: " + MyApplication.locations.get("tea-party"));
+//                            setAdapterContents();
+                        } else {
+//                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                }
+        );
+    }
+    //Update Order's status and ETA
+    private void updateStatus(int status, double ETA, Order order){
+        Map<String, Object> data = new HashMap<>();
+        data.put("deliveryManUserID",MyApplication.user.getIdentifier());
+        data.put("customerUserID",order.getCustomerUserID());
+        data.put("deliveryLocation",order.getDeliveryLocation());
+        data.put("locationID", order.getLocationID());
+        data.put("menuItems",order.getMenuItems());
+        data.put("status", status);
+        data.put("ETA", ETA);
+        MyApplication.db.collection("orders").document(order.getCustomerUserID()).set(data, SetOptions.merge())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                 Log.e(TAG, "User updated.");
+            }
+            })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                Log.e(TAG, "Error updating user", e);
+            }
+            });
+
+    }
+
+    public void goToFulfilAcceptedOrders(View view) {
+        Intent fulfilAcceptedOrders = new Intent(this, FulfilAcceptedOrdersActivity.class);
+        startActivity(fulfilAcceptedOrders);
     }
 }
